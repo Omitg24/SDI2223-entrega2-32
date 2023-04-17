7bc83964afcc092ab97bd4075aa1efc55fc2d4f3
Israel Solís Iglesias <72994140+IsraelSI11@users.noreply.github.com>
2023-04-17 20:51:36 +0200

    17/04/2023 - Israel - Base de la logica del logging

---
+++ urwalletpop/routes/logs.js
@@ -0,0 +1,35 @@
+module.exports = function (app, logsRepository) {
+
+    /**
+     * Metodo que obtiene los logs de la base de datos y si hay una busqueda obtiene los logs en funcion del texto
+     * y devuelve una vista que se encargara de mostrarlos
+     */
+    app.get('/log/list', function (req, res) {
+        let options={sort:{date:1}};
+        let filter={};
+
+        //Si la peticion recibe un texto de busqueda actualizamos el filtro
+        if(req.query.search != null && typeof(req.query.search) != "undefined" && req.query.search.trim() != "") {
+            filter = {"category": req.query.search};
+        }
+        logsRepository.getLogs(filter,options).then(logs=>{
+            let response={
+                logList: logs,
+            }
+            res.render("log/list.twig",response);
+        }).catch(error => {
+        });
+    });
+    /**
+     * Metodo que elimina todos los logs de la base de datos
+     */
+    app.get('/log/delete', function (req, res) {
+        logsRepository.deleteLogs().then(result => {
+            let response={
+                deletedCount: result.deletedCount,
+            }
+            res.render("log/list.twig",response);
+        }).catch(error => {
+        });
+    });
+}
