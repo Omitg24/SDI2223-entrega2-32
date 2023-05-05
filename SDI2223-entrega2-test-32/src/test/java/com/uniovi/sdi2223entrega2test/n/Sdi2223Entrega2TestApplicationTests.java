fecc25c4dc6de2f388f8c7ad408d70ad2d22a41c
UO284548 <uo284548@uniovi.es>
2023-05-05 10:12:49 +0200

    Test[26-27-28-29]: Comprar ofertas y listar ofertas compradas

--- SDI2223-entrega2-test-32/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
+++ SDI2223-entrega2-test-32/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
@@ -1,12 +1,20 @@
 package com.uniovi.sdi2223entrega2test.n;
 
+import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_HomeView;
+import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_PrivateView;
+import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_Properties;
+import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_View;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class Sdi2223Entrega2TestApplicationTests {
@@ -14,8 +22,8 @@ class Sdi2223Entrega2TestApplicationTests {
     //static String Geckodriver = "C:\\Path\\geckodriver-v0.30.0-win64.exe";
     static String Geckodriver = "C:\\Dev\\tools\\selenium\\geckodriver-v0.30.0-win64.exe";
     static String URL = "http://localhost:8081";    //static String PathFirefox = "/Applications/Firefox.app/Contents/MacOS/firefox-bin";
-//static String Geckodriver = "/Users/USUARIO/selenium/geckodriver-v0.30.0-macos";
-//Común a Windows y a MACOSX
+    //static String Geckodriver = "/Users/USUARIO/selenium/geckodriver-v0.30.0-macos";
+    //Común a Windows y a MACOSX
     static WebDriver driver = getDriver(PathFirefox, Geckodriver);
 
     public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
@@ -119,6 +127,125 @@ class Sdi2223Entrega2TestApplicationTests {
         Response response = RestAssured.get(RestAssuredURL);
         Assertions.assertEquals(403, response.getStatusCode());
     }
+
+    /**
+     * PR26. Sobre una búsqueda determinada (a elección del desarrollador),
+     * comprar una oferta que deja un saldo positivo en el contador del comprador.
+     * Comprobar que el contador se actualiza correctamente en la vista del comprador.
+     * Realizada por: Álvaro
+     */
+    @Test
+    @Order(26)
+    public void PR26() {
+        //Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user13@email.com", "user13");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/searchList')]", 0);
+        //Hacemos una busqueda
+        PO_PrivateView.makeSearch(driver, "117");
+        //Compramos la oferta
+        List<WebElement> elements = PO_View.checkElementBy(driver, "free", "//button[contains(text(), 'Comprar')]");
+        elements.get(0).click();
+        //Lo comparamos con el precio restado
+        elements = PO_View.checkElementBy(driver, "free", "//span[contains(@class, 'badge badge-secondary')]");
+        double result = Double.parseDouble(elements.get(0).getText());
+        Assertions.assertEquals(result, 30.31);
+
+        //Cierro sesion
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR27. Sobre una búsqueda determinada (a elección del desarrollador),
+     * comprar una oferta que deja un saldo 0 en el contador del comprador.
+     * Comprobar que el contador se actualiza correctamente en la vista del comprador.
+     * Realizada por: Álvaro
+     */
+    @Test
+    @Order(27)
+    public void PR27() {
+        //Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user11@email.com", "user11");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/searchList')]", 0);
+        //Hacemos una busqueda
+        PO_PrivateView.makeSearch(driver, "130");
+        //Compramos la oferta
+        List<WebElement> elements = PO_View.checkElementBy(driver, "free", "//button[contains(text(), 'Comprar')]");
+        elements.get(0).click();
+        //Lo comparamos con el precio restado
+        elements = PO_View.checkElementBy(driver, "free", "//span[contains(@class, 'badge badge-secondary')]");
+        double result = Double.parseDouble(elements.get(0).getText());
+        Assertions.assertEquals(result, 0.00);
+
+        //Cierro sesion
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR28. Sobre una búsqueda determinada (a elección del desarrollador),
+     * intentar comprar una oferta que esté por encima de saldo disponible del comprador.
+     * Y comprobar que se muestra el mensaje de saldo no suficiente
+     * Realizada por: Álvaro
+     */
+    @Test
+    @Order(28)
+    public void PR28() {
+        //Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user13@email.com", "user13");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/searchList')]", 0);
+        //Hacemos una busqueda
+        PO_PrivateView.makeSearch(driver, "25");
+        //Compramos la oferta
+        List<WebElement> elements = PO_View.checkElementBy(driver, "free", "//button[contains(text(), 'Comprar')]");
+        elements.get(0).click();
+        //Se muestra el mensaje de error
+        List<WebElement> result = PO_View.checkElementByKey(driver, "error.amount",
+                PO_Properties.getSPANISH());
+        String checkText = PO_HomeView.getP().getString("error.amount",
+                PO_Properties.getSPANISH());
+        Assertions.assertEquals(checkText, result.get(0).getText());
+        //Cierro sesion
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR29. Ir a la opción de ofertas compradas del usuario y mostrar la lista.
+     * Comprobar que aparecen las ofertas que deben aparecer
+     * Realizada por: Álvaro
+     */
+    @Test
+    @Order(29)
+    public void PR29() {
+        //Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user12@email.com", "user12");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/searchList')]", 0);
+        //Hacemos una busqueda
+        PO_PrivateView.makeSearch(driver, "20");
+        //Compramos la oferta
+        List<WebElement> elements = PO_View.checkElementBy(driver, "free", "//button[contains(text(), 'Comprar')]");
+        elements.get(0).click();
+        //Vamos a la pestaña de ofertas compradas
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de ofertas compradas: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/purchasedList')]", 0);
+        List<WebElement> offerList = driver.findElements(By.xpath("//div[contains(@class, 'card border-dark mb-3')]"));
+        // Comprobamos que se encuentren todas las ofertas
+        Assertions.assertEquals(1, offerList.size());
+        //Cierro sesion
+        PO_PrivateView.logout(driver);
+    }
+
 
     @Test
     @Order(38)
--- SDI2223-entrega2-test-32/src/test/java/com/uniovi/sdi2223entrega2test/n/pageobjects/PO_PrivateView.java
+++ SDI2223-entrega2-test-32/src/test/java/com/uniovi/sdi2223entrega2test/n/pageobjects/PO_PrivateView.java
@@ -1,26 +1,76 @@
 package com.uniovi.sdi2223entrega2test.n.pageobjects;
 
 import com.uniovi.sdi2223entrega2test.n.util.SeleniumUtils;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class PO_PrivateView extends PO_NavView {
-    static public void fillFormAddMark(WebDriver driver, int userOrder, String descriptionp, String scorep) {
-        //Esperamos 5 segundo a que carge el DOM porque en algunos equipos falla
-        SeleniumUtils.waitSeconds(driver, 5);
-        //Seleccionamos el alumnos userOrder
-        new Select(driver.findElement(By.id("user"))).selectByIndex(userOrder);
-        //Rellenemos el campo de descripción
-        WebElement description = driver.findElement(By.name("description"));
-        description.clear();
-        description.sendKeys(descriptionp);
-        WebElement score = driver.findElement(By.name("score"));
-        score.click();
-        score.clear();
-        score.sendKeys(scorep);
-        By boton = By.className("btn");
-        driver.findElement(boton).click();
+
+    /**
+     * Método para realizar el login de un usuario
+     *
+     * @param driver   driver
+     * @param email      dni del usuario
+     * @param password contraseña del usuario
+     */
+    static public void login(WebDriver driver, String email, String password) {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-dark");
+        PO_LoginView.fillLoginForm(driver, email, password);
+        PO_View.checkElementBy(driver, "text", email);
     }
+
+    /**
+     * Método para desconectar a un usuario
+     *
+     * @param driver driver
+     */
+    static public void logout(WebDriver driver) {
+        String loginText = PO_HomeView.getP().getString("msg.signup", PO_Properties.getSPANISH());
+        PO_PrivateView.clickOption(driver, "logout", "text", loginText);
+    }
+
+    /**
+     * Método que comprueba que un texto se encuentra en la vista
+     *
+     * @param driver    driver
+     * @param checkText texto a buscar
+     */
+    static public void checkElement(WebDriver driver, String checkText) {
+        List<WebElement> elements = PO_View.checkElementBy(driver, "text", checkText);
+        Assertions.assertEquals(checkText, elements.get(0).getText());
+    }
+
+    /**
+     * Método que compruba si un elemento se encuentra en la vista y realiza un click
+     *
+     * @param driver driver
+     * @param type   tipo del elemento
+     * @param text   Xpath del elemento
+     * @param index  indice del objeto que coincide con los datos pasados
+     */
+    static public void checkViewAndClick(WebDriver driver, String type, String text, int index) {
+        List<WebElement> elements = checkElementBy(driver, type, text);
+        elements.get(index).click();
+    }
+
+    /**
+     * Método que rellena el buscador de ofertas con el texto y realiza la busqueda
+     *
+     * @param driver     driver
+     * @param searchText texto a buscar
+     */
+    static public void makeSearch(WebDriver driver, String searchText) {
+        List<WebElement> elements = PO_View.checkElementBy(driver, "free", "//*[@id=\"searchTextForm\"]/div/div/input");
+        elements.get(0).click();
+        elements.get(0).sendKeys(searchText);
+        elements = PO_View.checkElementBy(driver, "free", "//*[@id=\"searchTextForm\"]/div/button");
+        elements.get(0).click();
+    }
+
+
 }
