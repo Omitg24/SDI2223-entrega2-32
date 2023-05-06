4d795f7a46bd15a0fd2a4d78d4ed9e676eed516f
uo278968 <uo278968@uniovi.es>
2023-05-06 20:10:33 +0200

    06/05/2023 - David - Terminadas pruebas

--- SDI2223-entrega2-test-32/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
+++ SDI2223-entrega2-test-32/src/test/java/com/uniovi/sdi2223entrega2test/n/Sdi2223Entrega2TestApplicationTests.java
@@ -1,9 +1,7 @@
 package com.uniovi.sdi2223entrega2test.n;
 
-import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_HomeView;
-import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_PrivateView;
-import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_Properties;
-import com.uniovi.sdi2223entrega2test.n.pageobjects.PO_View;
+import com.uniovi.sdi2223entrega2test.n.pageobjects.*;
+import com.uniovi.sdi2223entrega2test.n.util.SeleniumUtils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -29,6 +27,7 @@ class Sdi2223Entrega2TestApplicationTests {
     static WebDriver driver = getDriver(PathFirefox, Geckodriver);
     private MongoDB m;
 
+    private String BASE_API_URL ="http://localhost:8081/apiclient";
 
 
     public static WebDriver getDriver(String PathFirefox, String Geckodriver) {
@@ -144,7 +143,7 @@ class Sdi2223Entrega2TestApplicationTests {
      */
     @Test
     @Order(16)
-    public void PR16(){
+    public void PR16() {
         // Iniciamos sesión como usuario estandar
         PO_PrivateView.login(driver, "user01@email.com", "user01");
 
@@ -155,6 +154,8 @@ class Sdi2223Entrega2TestApplicationTests {
 
         // Rellenamos el formulario de alta de oferta con datos validos
         PO_PrivateView.fillFormAddOffer(driver, "PruebaTitulo", "PruebaDescripcion", "0.21");
+        // Vamos a la tercera pagina de la lista de ofertas propias del usuario
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 2);
 
         // Comprobamos que la oferta recien añadida sale en la lista de ofertas propias
         // del usuario
@@ -168,11 +169,11 @@ class Sdi2223Entrega2TestApplicationTests {
 
     /**
      * PR17. Ir al formulario de alta de oferta, rellenarla con datos inválidos (campo título vacío y precio
-     *  en negativo) y pulsar el botón Submit
+     * en negativo) y pulsar el botón Submit
      */
     @Test
     @Order(17)
-    public void PR17(){
+    public void PR17() {
         // Iniciamos sesión como usuario estandar
         PO_PrivateView.login(driver, "user01@email.com", "user01");
 
@@ -188,6 +189,207 @@ class Sdi2223Entrega2TestApplicationTests {
         PO_PrivateView.checkElement(driver, "Titulo de la oferta no puede estar vacío o tener menos de 3 caracteres");
         PO_PrivateView.checkElement(driver, "Precio de la oferta no puede ser negativo");
 
+        // Hacemos logout
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR18. Mostrar el listado de ofertas para dicho usuario y comprobar que se muestran todas las que
+     * existen para este usuario (10 ofertas).
+     */
+    @Test
+    @Order(18)
+    public void PR18() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user04@email.com", "user04");
+
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta propias: //a[contains(@href, 'offer/ownedList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/ownedList')]", 0);
+
+        //Guardamos el número de ofertas de la primera página
+        List<WebElement> offerList = SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout());
+        //Vamos a la segunda página
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 1);
+        offerList.addAll(SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout()));
+        // Comprobamos que se encuentren todas las ofertas
+        Assertions.assertEquals(10, offerList.size());
+        // Hacemos logout
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR19.  Ir a la lista de ofertas, borrar la primera oferta de la lista, comprobar que la lista se actualiza
+     * y que la oferta desaparece.
+     */
+    @Test
+    @Order(19)
+    public void PR19() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user04@email.com", "user04");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta propias: //a[contains(@href, 'offer/ownedList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/ownedList')]", 0);
+        //Borramos la primera oferta de la pagina
+        PO_PrivateView.checkViewAndClick(driver, "free",
+                "//h4[contains(text(), 'Oferta 41')]/following-sibling::*/a[contains(@href, 'offer/delete')]", 0);
+        //Comprobamos que ha desaparecido la oferta '41
+        SeleniumUtils.waitTextIsNotPresentOnPage(driver, "41", PO_View.getTimeout());
+        // Hacemos logout
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR20. Ir a la lista de ofertas, borrar la última oferta de la lista, comprobar que la lista se actualiza
+     * y que la oferta desaparece.
+     */
+    @Test
+    @Order(20)
+    public void PR20() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user04@email.com", "user04");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta propias: //a[contains(@href, 'offer/ownedList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/ownedList')]", 0);
+        //Vamos a la última pagina
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 1);
+        //Borramos la última oferta de la pagina
+        PO_PrivateView.checkViewAndClick(driver, "free",
+                "//h4[contains(text(), 'OFERTA 49')]/following-sibling::*/a[contains(@href, 'offer/delete')]", 0);
+        //Comprobamos que ha desaparecido la oferta '49'
+        SeleniumUtils.waitTextIsNotPresentOnPage(driver, "OFERTA 49", PO_View.getTimeout());
+        // Hacemos logout
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR21.  Ir a la lista de ofertas, borrar una oferta de otro usuario, comprobar que la oferta no se
+     * borra. Id de la oferta de user02@email.com con titulo 117
+     */
+    @Test
+    @Order(21)
+    public void PR21() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user04@email.com", "user04");
+        // Navegamos para borrar una oferta que no es nuestra oferta (Id oferta de user02@email.com, titulo 117)
+        driver.navigate().to("http://localhost:8081/offer/delete/645684b8c6cb6031f07f3af2");
+        // Comprobamos que se muestra el mensaje de error
+        PO_PrivateView.checkElement(driver, "La oferta no existe o no se encuentra disponible");
+    }
+
+    /**
+     * PR22. Ir a la lista de ofertas, borrar una oferta propia que ha sido vendida, comprobar que la
+     * oferta no se borra.
+     */
+    @Test
+    @Order(22)
+    public void PR22() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user04@email.com", "user04");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta propias: //a[contains(@href, 'offer/ownedList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/ownedList')]", 0);
+        //Borramos la oferta '44' que ya ha sido vendida
+        PO_PrivateView.checkViewAndClick(driver, "free",
+                "//h4[contains(text(), 'Oferta 44')]/following-sibling::*/a[contains(@href, 'offer/delete')]", 0);
+        // Comprobamos que se muestran los dos menasjes de error
+        PO_PrivateView.checkElement(driver, "La oferta no existe o no se encuentra disponible");
+        // Hacemos logout
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR23.  Hacer una búsqueda con el campo vacío y comprobar que se muestra la página que
+     * corresponde con el listado de las ofertas existentes en el sistema.
+     */
+    @Test
+    @Order(23)
+    public void PR23() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user10@email.com", "user10");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/searchList')]", 0);
+        //Dejamos el campo de busqueda vacio y buscamos
+        PO_PrivateView.makeSearch(driver, "");
+        //Guardamos los elemento de la primera pagina
+        List<WebElement> offerList = SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout());
+        //Vamos a la segunda página
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 1);
+        offerList.addAll(SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout()));
+        //Vamos a la tercera página
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 2);
+        offerList.addAll(SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout()));
+        //Recorremos las paginas guardando las ofertas
+        for (int i = 0; i < 26; i++) {
+            PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 3);
+            offerList.addAll(SeleniumUtils.waitLoadElementsBy(driver, "free",
+                    "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout()));
+        }
+        // Comprobamos que se encuentren todas las ofertas
+        Assertions.assertEquals(143, offerList.size());
+        // Hacemos logout
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR24. Hacer una búsqueda escribiendo en el campo un texto que no exista y comprobar que se
+     * muestra la página que corresponde, con la lista de ofertas vacía
+     */
+    @Test
+    @Order(24)
+    public void PR24() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user10@email.com", "user10");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/searchList')]", 0);
+        //Dejamos el campo de busqueda vacio y buscamos
+        PO_PrivateView.makeSearch(driver, "SistemasDistribuidos");
+        //Guardamos los elemento de la primera pagina
+        List<WebElement> offerList = driver.findElements(By.xpath("//div[contains(@class, 'card border-dark mb-3')]"));
+        // Comprobamos que se encuentren todas las ofertas
+        Assertions.assertEquals(0, offerList.size());
+        // Hacemos logout
+        PO_PrivateView.logout(driver);
+    }
+
+    /**
+     * PR25.  Hacer una búsqueda escribiendo en el campo un texto en minúscula o mayúscula y
+     * comprobar que se muestra la página que corresponde, con la lista de ofertas que contengan dicho
+     * texto, independientemente que el título esté almacenado en minúsculas o mayúscula
+     */
+    @Test
+    @Order(25)
+    public void PR25() {
+        // Iniciamos sesión como usuario estandar
+        PO_PrivateView.login(driver, "user10@email.com", "user10");
+        //Pinchamos en la opción de menú de ofertas: //li[contains(@id, 'offers-menu')]/a
+        PO_PrivateView.checkViewAndClick(driver, "free", "//li[contains(@class, 'nav-item dropdown')]/a", 0);
+        //Esperamos a que aparezca la opción de mostrar oferta: //a[contains(@href, 'offer/searchList')]
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@href, 'offer/searchList')]", 0);
+        //Buscamos el texto 'oferta 4' en el campo de busqueda
+        PO_PrivateView.makeSearch(driver, "oferta 4");
+        //Guardamos los elemento de la primera pagina
+        List<WebElement> offerList = SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout());
+        //Vamos a la segunda página
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 1);
+        offerList.addAll(SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout()));
+        // Comprobamos que se encuentren todas las ofertas
+        Assertions.assertEquals(10, offerList.size());
         // Hacemos logout
         PO_PrivateView.logout(driver);
     }
@@ -332,6 +534,9 @@ class Sdi2223Entrega2TestApplicationTests {
         // Rellenamos el formulario de alta de oferta con datos validos
         PO_PrivateView.fillFormAddOfferFeatured(driver, "Prueba37", "PruebaDescripcion37", "0.37");
 
+        // Vamos a la tercera pagina de la lista de ofertas propias del usuario
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 2);
+
         // Comprobamos que la oferta recien añadida sale en la lista de ofertas propias
         // del usuario
         PO_PrivateView.checkElement(driver, "Prueba37");
@@ -379,6 +584,9 @@ class Sdi2223Entrega2TestApplicationTests {
         // Rellenamos el formulario de alta de oferta con datos validos
         PO_PrivateView.fillFormAddOffer(driver, "Prueba37", "PruebaDescripcion37", "0.37");
 
+        // Vamos a la tercera pagina de la lista de ofertas propias del usuario
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 2);
+
         // Comprobamos que la oferta recien añadida sale en la lista de ofertas propias
         // del usuario
         PO_PrivateView.checkElement(driver, "Prueba37");
@@ -425,6 +633,9 @@ class Sdi2223Entrega2TestApplicationTests {
         // Rellenamos el formulario de alta de oferta con datos validos
         PO_PrivateView.fillFormAddOffer(driver, "Prueba37", "PruebaDescripcion37", "0.37");
 
+        // Vamos a la tercera pagina de la lista de ofertas propias del usuario
+        PO_PrivateView.checkViewAndClick(driver, "free", "//a[contains(@class, 'page-link')]", 2);
+
         // Comprobamos que la oferta recien añadida sale en la lista de ofertas propias
         // del usuario
         PO_PrivateView.checkElement(driver, "Prueba37");
@@ -446,7 +657,7 @@ class Sdi2223Entrega2TestApplicationTests {
     @Test
     @Order(38)
     public void PR38() {
-        final String RestAssuredURL = "http://localhost:8081/api/v1.0/users/login";
+        final String RestAssuredURL = "http://localhost:8081/api/users/login";
         //2. Preparamos el parámetro en formato JSON
         RequestSpecification request = RestAssured.given();
         JSONObject requestParams = new JSONObject();
@@ -460,5 +671,53 @@ class Sdi2223Entrega2TestApplicationTests {
         Assertions.assertEquals(200, response.getStatusCode());
     }
 
+    /**
+     * PR41. Mostrar el listado de ofertas para dicho usuario y comprobar que se muestran todas las que
+     * existen para este usuario.
+     */
+    @Test
+    @Order(41)
+    public void PR41() {
+        final String RestAssuredURL = "http://localhost:8081/api";
+        //Llamamos al servicio de login
+        RequestSpecification request = RestAssured.given();
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("email", "user01@email.com");
+        requestParams.put("password", "user01");
+        request.header("Content-Type", "application/json");
+        request.body(requestParams.toJSONString());
+        // Hacemos la petición
+        Response response = request.post(RestAssuredURL+"/users/login");
+        String token = response.jsonPath().getString("token");
+        // Llamamos al servicio de ofertas
+        RequestSpecification request2 = RestAssured.given();
+        request2.header("Content-Type", "application/json");
+        request2.header("token",token );
+        // Hacemos la petición para obtener el listado
+        Response response2 = request2.get(RestAssuredURL+"/offers");
+        // Guardamos todas las ofertas
+        List<Object> offers = response2.jsonPath().getList("offers");
+        // Comprobamos que se muestran todas las ofertas
+        Assertions.assertEquals(143, offers.size());
+        Assertions.assertEquals(200, response2.getStatusCode());
+    }
+
+    /**
+     * PR51.  Mostrar el listado de ofertas disponibles y comprobar que se muestran todas las que existen,
+     * menos las del usuario identificado.
+     */
+    @Test
+    @Order(51)
+    public void PR51() {
+        // Accedemos a la página de login
+        driver.get(BASE_API_URL+"/client.html?w=login");
+        // Rellenamos el formulario de login
+        PO_LoginView.fillLoginForm(driver, "user01@email.com", "user01");
+        //Guardamos todas las ofertas disponibles
+        List<WebElement> offerList = SeleniumUtils.waitLoadElementsBy(driver, "free",
+                "//div[contains(@class, 'card border-dark mb-3')]", PO_View.getTimeout());
+        // Comprobamos que se muestran todas las ofertas disponibles
+        Assertions.assertTrue(offerList.size() > 0);
+    }
 
 }
